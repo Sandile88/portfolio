0f458140829aa694a59d2a9d52d51831208d41a8
Sandile88 <sandilep.mremi@gmail.com>
2024-11-27 23:28:41 +0200

    changed button size and placing

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,9 +1,9 @@
 const Contact = () => {
     return (
-        <div className="grid sm:grid-cols-2 items-start gap-16 p-4 mx-auto max-w-4xl bg-white font-[sans-serif]">
+        <section className="grid sm:grid-cols-2 items-start gap-16 p-4 mx-auto max-w-4xl bg-white font-[sans-serif]">
             <div>
-                <h1 className="text-gray-800 text-3xl font-extrabold">Let's Talk</h1>
-                <p className="text-sm text-gray-500 mt-4">Have some big idea or brand to develop and need help? Then reach out we'd love to hear about your project  and provide help.</p>
+                <h1 className="text-gray-800 text-3xl font-extrabold">Let's chat</h1>
+                <p className="text-sm text-gray-500 mt-4">React out</p>
 
                 <div className="mt-12">
                     <h2 className="text-gray-800 text-base font-bold">Email</h2>
@@ -64,18 +64,23 @@ const Contact = () => {
             </div>
 
             <form className="ml-auto space-y-4">
-                <input type='text' placeholder='Name'
+                <input type='text' placeholder='Full Name'
                     className="w-full rounded-md py-3 px-4 bg-gray-100 text-gray-800 text-sm outline-blue-500 focus:bg-transparent" />
-                <input type='email' placeholder='Email'
+                <input type='email' placeholder='Email Address'
                     className="w-full rounded-md py-3 px-4 bg-gray-100 text-gray-800 text-sm outline-blue-500 focus:bg-transparent" />
                 <input type='text' placeholder='Subject'
                     className="w-full rounded-md py-3 px-4 bg-gray-100 text-gray-800 text-sm outline-blue-500 focus:bg-transparent" />
                 <textarea placeholder='Message' rows={6}
                     className="w-full rounded-md px-4 bg-gray-100 text-gray-800 text-sm pt-3 outline-blue-500 focus:bg-transparent"></textarea>
-                <button type='button'
-                    className="text-white bg-blue-500 hover:bg-blue-600 tracking-wide rounded-md text-sm px-4 py-3 w-full !mt-6">Send</button>
+                  <button type="button"
+                            className="mt-12 flex items-center justify-center text-sm lg:mr-auto max-lg:w-full rounded-lg px-4 py-3 tracking-wide text-white bg-blue-600 hover:bg-blue-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill='#fff' className="mr-2" viewBox="0 0 548.244 548.244">
+                                <path fill-rule="evenodd" d="M392.19 156.054 211.268 281.667 22.032 218.58C8.823 214.168-.076 201.775 0 187.852c.077-13.923 9.078-26.24 22.338-30.498L506.15 1.549c11.5-3.697 24.123-.663 32.666 7.88 8.542 8.543 11.577 21.165 7.879 32.666L390.89 525.906c-4.258 13.26-16.575 22.261-30.498 22.338-13.923.076-26.316-8.823-30.728-22.032l-63.393-190.153z" clip-rule="evenodd" data-original="#000000" />
+                            </svg>
+                            Send Message
+                        </button>
             </form>
-        </div>
+        </section>
 
     )
 }
