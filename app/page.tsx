e8c250b4c6d40ca5d6b788831c34ea3ba425bf2e
Sandile88 <sandilep.mremi@gmail.com>
2024-11-27 23:22:09 +0200

    added contact structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "../app/components/Header";
 import Hero from "../app/components/Hero";
+import Contact from "./components/Contact";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
     <Header/>
     <Hero/>
+    <Contact/>
     </>
   );
 }
